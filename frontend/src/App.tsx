55f6f2a9e8918d54bc1721df14b0454567f8bd7d
jaayeshe <jubairaayeshe4@gmail.com>
2024-04-07 23:10:03 -0230

    AddHotel rendering for authenticated users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,11 @@ import {
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 const App = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -44,6 +47,22 @@ const App = () => {
             </Layout>
           }
         />
+
+        {/* logic to render the logged in routes */}
+        {/* this route below is only available to someone who's logged in  already */}
+        {isLoggedIn && (
+          <>
+            <Route
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+              path="/add-hotel"
+            />
+          </>
+        )}
+
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
