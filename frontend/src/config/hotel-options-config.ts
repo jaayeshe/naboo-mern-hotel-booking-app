3d113198081e8d0004b80c6055188f6d0a6333c7
jaayeshe <jubairaayeshe4@gmail.com>
2024-04-18 16:27:21 -0230

    Implement Facility Selection Component

--- frontend/src/config/hotel-options-config.ts
+++ frontend/src/config/hotel-options-config.ts
@@ -15,6 +15,18 @@ export const hotelTypes = [
   "Pet Friendly",
   "Self Catering",
 ];
+//list of facilities
+
+export const hotelFacilities = [
+  "Free WiFi",
+  "Parking",
+  "Airport Shuttle",
+  "Family Rooms",
+  "Non-Smoking Rooms",
+  "Outdoor Pool",
+  "Spa",
+  "Fitness Center",
+];
 
 //these types cannot be created by the user, these types are provided
 //by the hotel booking website & the user who signs up has to select
