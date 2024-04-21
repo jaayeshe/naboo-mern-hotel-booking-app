e0a98e4969ca25adef0570900ef9a08711085480
jaayeshe <jubairaayeshe4@gmail.com>
2024-04-20 23:22:51 -0230

    Creating new FormData object

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -22,14 +22,31 @@ export type HotelFormData = {
 const ManageHotelForm = () => {
   const formMethods = useForm<HotelFormData>();
   const { handleSubmit } = formMethods;
-  // we are destructuring the handleSubmit fucntion from formMethods, which we get from use form hook...
-  // so the handleSubmit function will submit the form & handle any validation & then pass the data to...
-  // our function which we'll define later.
+  // we are destructuring the handleSubmit function from formMethods, which we get from use form hook so the handleSubmit function will submit the form & handle any validation & then pass the data to our function which we'll define later.
 
-  const onSubmit = handleSubmit((formData: HotelFormData) => {
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
     // create a new FormData object & call our API
-    console.log(formData);
+    // console.log(formData);
+    const formData = new FormData();
+    formData.append("name", formDataJson.name);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
+    formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+    formData.append("starRating", formDataJson.starRating.toString());
+    formData.append("adultCount", formDataJson.adultCount.toString());
+    formData.append("childCount", formDataJson.childCount.toString());
+
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    });
   });
+
   return (
     <FormProvider {...formMethods}>
       <form className="flex flex-col gap-10" onSubmit={onSubmit}>
@@ -55,4 +72,5 @@ export default ManageHotelForm;
 
 // we need to use a Form Provider,
 //so our child component can access to all the React Hook Form Methods
+
 //
