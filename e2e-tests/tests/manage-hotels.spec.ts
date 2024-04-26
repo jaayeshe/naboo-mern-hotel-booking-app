239460bb783bc939a0df6313c0e19faa4b226fe2
jaayeshe <jubairaayeshe4@gmail.com>
2024-04-25 23:22:22 -0230

    Adding Tests for Add-Hotel page

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import path from "path";
 
 const UI_URL = "http://localhost:5173/";
 
@@ -10,11 +11,11 @@ test.beforeEach(async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-  //   await page.locator("[name=email]").fill("1@1.com");
-  //   await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password123");
 
-  await page.locator("[name=email]").fill("add@gmail.com");
-  await page.locator("[name=password]").fill("ayeshe");
+  // await page.locator("[name=email]").fill("add@gmail.com");
+  // await page.locator("[name=password]").fill("ayeshe");
 
   await page.getByRole("button", { name: "Login" }).click();
 
@@ -22,7 +23,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("should allow user to add a hotel", async ({ page }) => {
-  await page.goto(`${UI_URL}/add-hotel`);
+  await page.goto(`${UI_URL}add-hotel`);
 
   await page.locator('[name="name"]').fill("Test Hotel");
   await page.locator('[name="city"]').fill("Test City");
@@ -33,13 +34,21 @@ test("should allow user to add a hotel", async ({ page }) => {
   await page.locator('[name="pricePerNight"]').fill("100");
   await page.selectOption('select[name="starRating"]', "3");
 
-  await page.getByText("Budget").click();
+  await page.getByText("Romantic").click();
 
   await page.getByLabel("Free WiFi").check();
   await page.getByLabel("Parking").check();
 
   await page.locator('[name="adultCount"]').fill("2");
   await page.locator('[name="childCount"]').fill("4");
+
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "1.jpg"),
+    path.join(__dirname, "files", "2.jpg"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel saved")).toBeVisible();
 });
 // this is where we'll put our addHotel tests & then...
 // whenever we come to edit hotel test we put that in here as well.
