f72201f3cf49b53ea1dff1ca9f513184fcc90b9c
jaayeshe <jubairaayeshe4@gmail.com>
2024-05-17 23:59:44 -0230

    My Hotels Page done

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import * as apiClient from "../api-client";
 import { BsBuildingFill, BsMapFill } from "react-icons/bs";
-import { BiHotel, BiMoney } from "react-icons/bi";
+import { BiHotel, BiMoney, BiStar } from "react-icons/bi";
 
 const MyHotels = () => {
   const { data: hotelData } = useQuery(
@@ -21,7 +21,7 @@ const MyHotels = () => {
         <h1 className="text-3xl font-bold">My Hotels</h1>
         <Link
           to="/add-hotel"
-          className="flex bg-teal-700 text-white text-xl font-bold p-2 hover:bg-teal-600 "
+          className="flex bg-teal-700 text-white text-xl font-bold p-2 hover:bg-teal-600 rounded-xl"
         >
           Add Hotel
         </Link>
@@ -31,7 +31,8 @@ const MyHotels = () => {
           <div className="flex flex-col justify-between border border-slate-300 rounded-lg p-8 gap-5">
             <h2 className="text-2xl font-bold">{hotel.name}</h2>
             <div className="whitespace-pre-line">{hotel.description}</div>
-            <div className="grid grid-cols gap-2">
+
+            <div className="grid grid-cols-5 gap-2">
               <div className="border border-slate-300 rounded-sm p-3 flex items-center">
                 <BsMapFill className="mr-1" />
                 {hotel.city}, {hotel.country}
@@ -48,9 +49,22 @@ const MyHotels = () => {
 
               <div className="border border-slate-300 rounded-sm p-3 flex items-center">
                 <BiHotel className="mr-1" />
-                {hotel.adultCount} adults
+                {hotel.adultCount} adults, {hotel.childCount} children
+              </div>
+
+              <div className="border border-slate-300 rounded-sm p-3 flex items-center">
+                <BiStar className="mr-1" />
+                {hotel.starRating} star rating
               </div>
             </div>
+            <span className="flex justify-end">
+              <Link
+                to={`/edit-hotel/${hotel._id}`}
+                className="flex bg-teal-700 text-white text-l font-bold p-2 hover:bg-teal-600 rounded-xl"
+              >
+                View Details
+              </Link>
+            </span>
           </div>
         ))}
       </div>
@@ -70,3 +84,7 @@ export default MyHotels;
 // price per night,
 // no. of children, adults,
 // hotel star rating
+
+// whenever we click the view details link, we're going to redirect the user to the
+// edit hotels page & we're going to pass the hotel id in the URL so that on the edit hotel page we can have
+// some code that parses the URL, Gets the ID, & fetches that hotel
